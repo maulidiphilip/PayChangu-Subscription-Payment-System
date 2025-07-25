95ae511fa1fe6dfc4d487033a11f3c5559043904
maulidiphilip <maulidiphilip@gmailcom>
2025-07-25 14:27:22 +0200

    Made production ready

--- frontend/src/components/SubscriptionForm.jsx
+++ frontend/src/components/SubscriptionForm.jsx
@@ -13,7 +13,7 @@ const SubscriptionForm = () => {
 
   const handlePayment = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/payment/subscribe', {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/payment/subscribe`, {
         amount: planAmounts[plan],
         plan,
       });
